a28ad1a207bd79e98d6c6b6817a69d6bc826c546
Gilles F. Gonthier <gilles.f.gonthier@gmail.com>
2018-03-31 15:13:36 -0400

    Changed stepper direction for X and E0

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -529,8 +529,9 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 4000, 500 }
-
+//#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 4000, 500 }
+//#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.3190, 80.3190, 399.2901, 400 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.3190 * 2, 80.3190 * 2, 399.2901 *2 , 400 * 2 }
 /**
  * Default Max Feed Rate (mm/s)
  * Override with M203
@@ -748,7 +749,7 @@
 // @section machine
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
-#define INVERT_X_DIR false
+#define INVERT_X_DIR true
 #define INVERT_Y_DIR true
 #define INVERT_Z_DIR false
 
@@ -758,7 +759,7 @@
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR false
+#define INVERT_E0_DIR true
 #define INVERT_E1_DIR false
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
