865672113d5087c9454c81ac90befb34fb641988
Gilles F. Gonthier <gilles.f.gonthier@gmail.com>
2018-04-01 13:06:27 -0400

    Changed many parameters

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -119,7 +119,8 @@
 // The following define selects which electronics board you have.
 // Please choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_RAMPS_14_EFB
+  // gfg #define MOTHERBOARD BOARD_RAMPS_14_EFB
+  #define MOTHERBOARD BOARD_MKS_13
 #endif
 
 // Optional custom name for your RepStrap or other custom machine
@@ -783,8 +784,11 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 200
-#define Y_BED_SIZE 200
+// 
+// gfg #define X_BED_SIZE 200
+#define X_BED_SIZE 300
+// gfg #define Y_BED_SIZE 200
+#define Y_BED_SIZE 300
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -792,6 +796,8 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
+
+// gfg #define Z_MAX_POS 200
 #define Z_MAX_POS 200
 
 /**
